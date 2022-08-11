31fabde241de53eed8ee076aac16a0ed2eb1bbe1
Luciano Victor <lucianocarvallho7@gmail.com>
2022-08-11 15:21:33 -0300

    feat: adding section ProfileData and info user

--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../../components/ProfileData';
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+            username={'lucianovictor'}
+            name={'Luciano Victor'}
+            avatarUrl={'https://avatars.githubusercontent.com/u/54756923?v=4'}
+            followers={3000}
+            following={13}
+            company={undefined}
+            location={'São Paulo, Brazil'}
+            email={'lucianocarvallho7@gmail.com'}
+            blog={undefined}
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
 }
 
 export default Profile;
