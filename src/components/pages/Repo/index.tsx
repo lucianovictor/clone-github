232cc1303f3214aa0a946698ca198ecd2e69f5ff
Luciano Victor <lucianocarvallho7@gmail.com>
2022-08-19 10:24:00 -0300

    feat: adding section page repo

--- src/components/pages/Repo/index.tsx
+++ src/components/pages/Repo/index.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {Link} from 'react-router-dom'
+
+import { Container, Breadcrumb, RepoIcon, Stats, StarIcon, ForkIcon, LinkButton, GithubIcon  } from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={'username'} to={'/lucianovictor'}>
+          lucianovictor
+        </Link>
+          <span>/</span>
+        <Link className={'reponame'} to={'/lucianovictor/page-blizzard'}>
+          Page-Blizzard
+        </Link>
+      </Breadcrumb>
+        <p>project made using next, sass and typescript based on study</p>
+        <Stats>
+          <li>
+            <StarIcon />
+            <b>9</b>
+            <span>stars</span>
+          </li>
+          <li>
+            <ForkIcon />
+            <b>0</b>
+            <span>forks</span>
+          </li>
+        </Stats>
+
+        <LinkButton href={'https://github.com/lucianovictor/page-blizzard'}>
+          <GithubIcon />
+          <span>View on GitHub</span>
+        </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
